0568db8cdab813ec4a7be45167cc3fc5129ded89
Kirill Lavrov <lkv@myttk.ru>
2024-09-03 20:53:12 +0700

    continuation of experiments with cookies

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,6 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.cookies);
+
   res.setHeader("Set-Cookie", `session='deleted'; path=/; Expires=${new Date(0).toUTCString()}`);
   res.status(200).json({ user: { name: "" }, expires: null });
 }
